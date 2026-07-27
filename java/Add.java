c0cda6af57ed8188fc76bd36b8364972859a79e9
Arjun-patil-631 <5631arjunpatil@gmail.com>
2026-07-27 13:11:00 +0530

    today's notes --27-07

---
+++ java/Add.java
@@ -0,0 +1,15 @@
+public class Add{
+    public static void main(String[] args){
+        int result=plus(10,20);
+        System.out.printlln(result);
+    }
+    public static int plus(int a, int b){
+        return a+b;
+    }
+    public static void plus(int i, int j, int x){
+        
+    }
+    public static int plus(long l1, long l2){
+        return (int)(l1+l2);
+    }
+}
