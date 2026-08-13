e4d3adbd8c8af83fab9dc812a3d6d8f619cd4cbe
Arjun-patil-631 <5631arjunpatil@gmail.com>
2026-08-13 20:29:58 +0530

    understood more detailed about constructors and "this" keyword

--- java/DemoClock.java
+++ java/DemoClock.java
@@ -1,6 +1,17 @@
 public class DemoClock{
     public static void main(String[] args){
+        System.out.println("checking the constructor");
         clock c=new clock();
+        c.showTime();
+        System.out.println("\nchecking the  parametarized constructor");
+        clock d=new clock(12,12,13);
+        c.showTime();
+        System.out.println("\nchecking the single parameter constructor");
+        clock e =new clock(10);
+        c.showTime();
+        System.out.println("checking if the clock goes beyond 24 hrs");
+        clock f=new clock(25,32,21);
         c.showTime();
     }
 }
+
--- java/clock.java
+++ java/clock.java
@@ -2,7 +2,29 @@ public class clock{
     private int hours;
     private int minutes;
     private int seconds;
+                                                                             
+    public clock(){
+        this(10 , 10, 35);
 
+    }
+    public clock(int value){
+        this(value,value,value);
+    }
+    public clock(int hours, int minutes, int seconds){
+        if(hours>=0 && hours<=23 && minutes>=0 && minutes<=59 && seconds>=0 && seconds<=59){
+            this.hours = hours;
+            this.minutes = minutes;
+            this.seconds = seconds;
+        }
+        else{
+            throw new IllegalArgumentException("invalid values to time......");
+        }
+    }
+    public void setTime(int h, int m, int s){
+        hours = h;
+        minutes = m;
+        seconds = s;
+    }
     public void showTime(){
         System.out.println(hours+"hh "+minutes+"mm "+seconds+"ss ");
     }
