a70575e55d3bad21e56274b5cc3aadc1292aa531
Arjun-patil-631 <5631arjunpatil@gmail.com>
2026-09-13 12:23:56 +0530

    pattern Generator +next questions

--- java/lab internal 1/1st questions/patternGenerator.java
+++ java/lab internal 1/1st questions/patternGenerator.java
@@ -4,8 +4,24 @@ import java.util.Scanner;
 public class patternGenerator {
     public static void main(String[] args) {
         
-        System.out.println("Enter the Number N: ");
         Scanner sc = new Scanner(System.in);
-        System.out.print("");
+        System.out.println("Enter the Number N: ");
+        int N=sc.nextInt();
+
+        System.out.print("1st pattern: \n");
+        for(int i=N; i>0; i--){
+            for(int j=1; j<=i; j++){
+                System.out.print("* ");
+            }
+            System.out.print("\n");
+        }
+
+        System.out.println("\n2nd pattern: \n");
+        for (int i = 1; i <=N; i++) {
+            for(int j=1; j<= i; j++){
+                System.out.print(j);
+            }
+            System.out.print("\n");
+        }
     }
 }
